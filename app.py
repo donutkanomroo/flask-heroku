fecd98d30c71a1e459c2267f4ff8f12f786c0283
donutkanomroo <89238246+donutkanomroo@users.noreply.github.com>
2021-09-03 11:43:44 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello เปรมากร ทองคำ เลขที่ 12 ม.4/7"
+    return "Hello เปรมากร ทองคำ เลขที่ 12 ชั้น ม.4/7"
 
 @app.route('/api', methods=['GET'])
 def get_api():
